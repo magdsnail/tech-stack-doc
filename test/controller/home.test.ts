e925393d8a2a33455926feac5e4cd041dc911b23
jason.wang <wangkjim@gmail.com>
2022-06-10 13:14:45 +0800

    ..

--- test/controller/home.test.ts
+++ test/controller/home.test.ts
@@ -12,7 +12,7 @@ describe('test/controller/home.test.ts', () => {
 
     // use expect by jest
     expect(result.status).toBe(200);
-    expect(result.text).toBe('Hello Midwayjs!');
+    expect(result.body.data).toBe('//www.baidu.com/img/bd_logo1.png');
 
     // close app
     await close(app);
